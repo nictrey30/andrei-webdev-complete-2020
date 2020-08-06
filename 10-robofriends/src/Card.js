9fd8d956bc609e3fd8f627989584a46713ca8d41
Traian <niculescu.traian.ct@gmail.com>
2020-08-06 23:46:58 +0300

    update robofriends

---
+++ 10-robofriends/src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div>
+      <img src='' alt='' />
+      <div>
+        <h2>James Doe</h2>
+        <p>james.doe@gmail.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
