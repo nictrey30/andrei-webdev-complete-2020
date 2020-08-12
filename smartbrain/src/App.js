2ba6d93752b6aa45388d2eeee7781df52173ca45
Traian <niculescu.traian.ct@gmail.com>
2020-08-13 02:40:22 +0300

    smartbrain FE update

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai';
+
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '75c58209731d408db47489b1c2446c7c'
+});
 
 const particlesOptions = {
   particles: {
@@ -18,19 +25,57 @@ const particlesOptions = {
   }
 };
 
-function App() {
-  return (
-    <div className='App'>
-      <Particles className='particles' params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* 
-      <FaceRecognition /> 
-      */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      // imgUrl should be displayed when we click on submit
+      imgUrl: ''
+    };
+  }
+  // using arrow functions to avoid bind
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imgUrl: this.state.input });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function (response) {
+        // do something with response
+        // console.log(
+        //   response.outputs[0].data.regions[0].region_info.bounding_box
+        // );
+        const { regions } = response.outputs[0].data;
+        regions.forEach((region, index) => {
+          console.log(`Region ${index}`);
+          Object.entries(
+            region.region_info.bounding_box
+          ).forEach(([key, value]) => console.log(key, value));
+        });
+      },
+      function (err) {
+        // there was an error
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imgUrl={this.state.imgUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
