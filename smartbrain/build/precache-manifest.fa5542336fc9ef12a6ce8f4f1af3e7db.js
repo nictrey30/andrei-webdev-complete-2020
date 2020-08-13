682a5cb107e112399bd7ff62f5620db20d3fabe2
Traian <niculescu.traian.ct@gmail.com>
2020-08-13 15:22:37 +0300

    added build folder from smartbrain , commented out in .gitignore

---
+++ smartbrain/build/precache-manifest.fa5542336fc9ef12a6ce8f4f1af3e7db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a74335af194366163316a14faa7a9a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d4ed77e4ab9ec1bc3ce",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9ab15e94120bd6b91413",
+    "url": "/static/css/main.9d731ddb.chunk.css"
+  },
+  {
+    "revision": "5d4ed77e4ab9ec1bc3ce",
+    "url": "/static/js/2.b06bc4f0.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.b06bc4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab15e94120bd6b91413",
+    "url": "/static/js/main.2f19b09b.chunk.js"
+  },
+  {
+    "revision": "8fae2f0bf44244f6ee82",
+    "url": "/static/js/runtime-main.14ada470.js"
+  }
+]);
