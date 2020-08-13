abdc79a4957662b152da4a3d119f3de73e7ae95c
Traian <niculescu.traian.ct@gmail.com>
2020-08-13 15:16:25 +0300

    update FE with first stage signin and signout

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
 import Rank from './components/Rank/Rank';
 import Clarifai from 'clarifai';
 
@@ -31,7 +32,11 @@ class App extends Component {
     this.state = {
       input: '',
       // imgUrl should be displayed when we click on submit
-      imgUrl: ''
+      imgUrl: '',
+      // boxes - an object that will contain the boxes with their respective coord that we receive for each face detected in the picture
+      box: {},
+      // route keeps track of where we are on the page
+      route: 'signin'
     };
   }
   // using arrow functions to avoid bind
@@ -39,40 +44,59 @@ class App extends Component {
     this.setState({ input: event.target.value });
   };
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  };
+
   onButtonSubmit = () => {
     this.setState({ imgUrl: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function (response) {
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
         // do something with response
-        // console.log(
-        //   response.outputs[0].data.regions[0].region_info.bounding_box
-        // );
-        const { regions } = response.outputs[0].data;
-        regions.forEach((region, index) => {
-          console.log(`Region ${index}`);
-          Object.entries(
-            region.region_info.bounding_box
-          ).forEach(([key, value]) => console.log(key, value));
-        });
-      },
-      function (err) {
-        // there was an error
-      }
-    );
+        // response.outputs[0].data.regions[0].region_info.bounding_box
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch((err) => console.log(err));
+  };
+
+  onRouteChange = (route) => {
+    this.setState({ route: route });
   };
 
   render() {
     return (
       <div className='App'>
         <Particles className='particles' params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition imgUrl={this.state.imgUrl} />
+        <Navigation onRouteChange={this.onRouteChange} />
+        {this.state.route === 'signin' ? (
+          <SignIn onRouteChange={this.onRouteChange} />
+        ) : (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition imgUrl={this.state.imgUrl} box={this.state.box} />
+          </div>
+        )}
       </div>
     );
   }
--- smartbrain/src/components/FaceRecognition/FaceRecognition.js
+++ smartbrain/src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imgUrl }) => {
+const FaceRecognition = ({ imgUrl, box }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img src={imgUrl} alt='' width='500px' height='auto' />
+        <img id='inputimage' src={imgUrl} alt='' width='500px' height='auto' />
+        <div
+          className='bounding-box'
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            left: box.leftCol,
+            bottom: box.bottomRow
+          }}
+        ></div>
       </div>
     </div>
   );
