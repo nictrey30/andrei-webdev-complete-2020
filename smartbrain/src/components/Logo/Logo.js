6c0e8973ee8f7c6b2f13a1f062776ac6769c7e04
Traian <niculescu.traian.ct@gmail.com>
2020-09-19 11:37:58 +0300

    update

--- smartbrain/src/components/Logo/Logo.js
+++ smartbrain/src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import Tilt from 'react-tilt';
 import './Logo.css';
 import brain from './brain.png';
 
-function Logo() {
+const Logo = () => {
   return (
     <div className='ma4 mt0'>
       <Tilt
